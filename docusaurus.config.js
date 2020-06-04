38c0f93215744e9b383876d5a7bb52f009e86abb
Eric ACKERMANN <ackermanneric@gmail.com>
2020-06-04 19:09:27 +0200

    Set config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,26 +3,26 @@ module.exports = {
   tagline: "The tagline of my site",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
-  favicon: "img/favicon.ico",
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  favicon: "img/javascript.ico",
+  organizationName: "Eric ACKERMANN", // Usually your GitHub org/user name.
+  projectName: "js-fullstack-doc", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: "My Site",
+      title: "This is JavaScript (or what I know of)",
       logo: {
         alt: "My Site Logo",
-        src: "img/logo.svg",
+        src: "img/javascript.ico",
       },
       links: [
         {
           to: "docs/",
           activeBasePath: "docs",
           label: "Docs",
-          position: "left",
+          position: "right",
         },
         // {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/eric-ackermann/js-fullstack-doc",
           label: "GitHub",
           position: "right",
         },
@@ -64,18 +64,27 @@ module.exports = {
         {
           title: "More",
           items: [
+            // {
+            //   label: "Blog",
+            //   to: "blog",
+            // },
             {
-              label: "Blog",
-              to: "blog",
+              label: "Personnal Site",
+              href: "https://github.com/eric-ackermann/personnal-site",
+            },
+
+            {
+              label: "Github",
+              href: "https://github.com/eric-ackermann",
             },
             {
-              label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              label: "Docusaurus site",
+              href: "https://v2.docusaurus.io/",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} made by Eric ACKERMANN. Built with Docusaurus.`,
     },
   },
   presets: [
@@ -87,8 +96,7 @@ module.exports = {
           homePageId: "doc1",
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/",
+          editUrl: "https://github.com/Eric-ACKERMANN/js-fullstack-doc",
         },
         blog: {
           showReadingTime: true,
